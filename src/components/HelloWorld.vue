25ff088c9136dd47850578e832d6a84406cdf7dd
zenghao-nb <59545524+zenghao-nb@users.noreply.github.com>
2020-04-26 17:31:14 +0800

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -24,6 +24,8 @@
     <h3>Ecosystem</h3>
     <h2>曾浩</h2>
     <h2>曾浩</h2>
+    <h2>曾浩</h2>
+    <h2>曾浩</h2>
     <ul>
       <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
       <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
