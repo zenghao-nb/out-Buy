ff7d31eb9c10cd18cd1ede36ab1fc4605037f49d
zenghao-nb <59545524+zenghao-nb@users.noreply.github.com>
2020-04-27 15:27:37 +0800

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,25 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+<<<<<<< HEAD
     <div>sdfjklsj</div>
+=======
+    <el-alert
+            title="成功提示的文案"
+            type="success">
+    </el-alert>
+    <el-alert
+            title="消息提示的文案"
+            type="info">
+    </el-alert>
+    <el-alert
+            title="警告提示的文案"
+            type="warning">
+    </el-alert>
+    <el-alert
+            title="错误提示的文案"
+            type="error">
+    </el-alert>
+>>>>>>> zh-branch
   </div>
 </template>
