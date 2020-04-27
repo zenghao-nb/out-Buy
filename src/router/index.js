b5dbec34637e847c3c0128cf903e8d85c023e7d0
zenghao-nb <59545524+zenghao-nb@users.noreply.github.com>
2020-04-27 13:01:19 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,12 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+    {
+      path: '/my',
+      name: 'My',
+      component: () => import(/* webpackChunkName: "about" */ '../views/My.vue')
+    }
 ]
 
 const router = new VueRouter({
