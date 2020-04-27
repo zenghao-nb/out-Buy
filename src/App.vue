74bee5f97d3c14efe5d60e0a2660f9459a3e1352
zenghao-nb <59545524+zenghao-nb@users.noreply.github.com>
2020-04-27 15:25:44 +0800

    Update App.vue
    2020.4.27

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,39 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>|
-      <router-link to="/my">我的</router-link>
+      <router-link to="/">
+        <i class="el-icon-s-home"></i>
+        <span>首页</span>
+      </router-link>
+      <router-link to="/about">
+        <i class="el-icon-s-home"></i>
+        <span>订单</span>
+        </router-link>
+      <router-link to="/my">
+        <i class="el-icon-s-home"></i>
+        <span>我的</span>
+      </router-link>
     </div>
+<!--      <tableBar></tableBar>-->
     <router-view/>
   </div>
 </template>
-
+<script>
+    // import tableBar from './components/zh/table-bar'
+    // components:{
+    //     tableBar
+    // }
+</script>
 <style lang="less" >
-  @font:14px;
-  @color:yellow;
+  @font:20px;
+  @color: #ffe32b;
+  @screenHeight:60px;
+  @a:pink
   *{
     margin: 0;
     padding: 0;
   }
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,19 +42,27 @@
   color: #2c3e50;
   /*position: relative;*/
 }
-
 #nav {
-  padding: 30px;
+  height: @screenHeight;
+  max-width: 750px;
+  min-width: 320px;
   position: fixed;
-  left: 0;
- bottom:0px;
   width: 100%;
+  left: 0;
+  bottom: 0;
   font-size: @font;
   background-color: @color;
+  z-index: 5;
+  display: flex;
+  justify-content: space-around;
 
   a {
+    margin: 10px 0;
+    text-decoration: none;
     font-weight: bold;
-    color: #2c3e50;
+    color: #567264;
+    display: flex;
+    flex-direction: column;
 
     &.router-link-exact-active {
       color: #42b983;
