cd1e9d685c8dede7f0f054a36a112a52deba6f90
zenghao-nb <59545524+zenghao-nb@users.noreply.github.com>
2020-04-27 13:01:31 +0800

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <el-alert
+            title="成功提示的文案"
+            type="success">
+    </el-alert>
+    <el-alert
+            title="消息提示的文案"
+            type="info">
+    </el-alert>
+    <el-alert
+            title="警告提示的文案"
+            type="warning">
+    </el-alert>
+    <el-alert
+            title="错误提示的文案"
+            type="error">
+    </el-alert>
   </div>
 </template>
