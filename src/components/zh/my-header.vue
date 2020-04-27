8e388c2185b5fba27ab56bd6b531d86e72a77809
zenghao-nb <59545524+zenghao-nb@users.noreply.github.com>
2020-04-27 13:00:56 +0800

    Create my-header.vue

---
+++ src/components/zh/my-header.vue
@@ -0,0 +1,54 @@
+<template>
+</template>
+
+<script>
+    export default {
+        name: "my-header",
+        data(){
+            return{
+                username:'我是用户'
+            }
+        }
+    }
+</script>
+
+<style scoped lang="less">
+    .header-top {
+        display: flex;
+        justify-content: space-between;
+        margin-top: 10px;
+    }
+
+    .user-infor {
+        display: flex;
+        margin: auto 20px;
+
+    div:nth-child(2) {
+        margin: auto 10px;
+    }
+
+    img {
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+    }
+    }
+    .shezhi {
+        display: flex;
+        margin: 20px 20px;
+
+    div:first-child {
+        margin-right: 20px;
+    }
+    }
+
+    .header-bottom {
+        display: flex;
+        justify-content: space-around;
+        margin: 20px 0;
+    &>div{
+         display: flex;
+         flex-direction: column;
+     }
+    }
+</style>
