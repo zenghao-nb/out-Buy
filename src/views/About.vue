7fa5b7a3f1744b87f4e41b93a2af266c93e2ebfa
tian996 <1714925579@qq.com>
2020-04-27 18:36:20 +0800

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,10 @@
 <template>
+
   <div class="about">
     <h1>This is an about page</h1>
-<<<<<<< HEAD
+
     <div>sdfjklsj</div>
-=======
+
     <el-alert
             title="成功提示的文案"
             type="success">
@@ -21,5 +22,22 @@
             type="error">
     </el-alert>
 >>>>>>> zh-branch
+=======
+  <div class="home">
+    <Tablebar msg="Welcome to Your Vue.js App"/>
+>>>>>>> mxm-branch
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import Tablebar from '@/components/About/Tablebar.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Tablebar
+  }
+}
+</script>
+
