430fa41d0dd6d3b9b8ca884f906524ed280611c9
tian996 <1714925579@qq.com>
2020-04-27 18:33:57 +0800

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="home">
+    <Tablebar msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import Tablebar from '@/components/About/Tablebar.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Tablebar
+  }
+}
+</script>
+
