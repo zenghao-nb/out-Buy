4c78f3d12e3d5237d5ba6cd3bf6c60842e347ef8
tian996 <1714925579@qq.com>
2020-04-29 10:29:37 +0800

    Create Order.vue

---
+++ src/views/Mxm/Order.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <Tablebar msg="Welcome to Your Vue.js App"/>   
+  </div>
+</template>
+
+<script>
+import Tablebar from '@/components/Mxm/Tablebar.vue'
+export default {
+  name: 'Order',
+  components: {
+    Tablebar
+  }
+}
+</script>
+
